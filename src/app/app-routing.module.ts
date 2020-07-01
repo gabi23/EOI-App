941cb4f56c76a80b7bd2ac4a7b4dc6485cbf5495
Guzman Hernandez <guzmanhernandez90@gmail.com>
2020-07-01 01:14:29 +0100

    Añade ruta a perfil de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './pages/user/user.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users/:id', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
