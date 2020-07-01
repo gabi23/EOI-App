a7f8caec4b7aefcf164a1f9a9aa365b628fa98b0
gabi23 <gabi2332@gmail.com>
2020-07-01 00:15:03 +0000

    Se define el tipo User y se crea la función que permite obtener todos los usuarios de la db

--- src/app/services/api-manager.service.ts
+++ src/app/services/api-manager.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+export type User = { 
+  name: string, 
+  email: string, 
+  type: string, 
+  safeWord: string,  
+  courses: string []
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiManagerService {
 
+  url = "http://localhost:3000/users";
+
   constructor() { }
+
+  getAllUsers(): Promise<User[]> {
+    const users = axios.get(this.url)
+                    .then(response => response.data)
+                    .catch(error => console.log(error));
+    return users;
+  }
+
+
 }
