16b034c61c1e6b114242ba81e8ba8cf21262b6ae
gabi23 <gabi2332@gmail.com>
2020-07-01 00:17:30 +0000

    Se crea la función 'loadUsers', que obtiene todos los usuarios de la db llamando a la función correspondiente del servicio

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User, ApiManagerService } from '../../services/api-manager.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
 
-  ngOnInit(): void {
+  constructor(private apiManagerServices: ApiManagerService) {
+    this.loadUsers();
+  }
+
+  ngOnInit(): void { }
+
+  async loadUsers(): Promise<void> {
+    /* const id = this.route.snapshot.paramMap.get('id'); */
+    const users = await this.apiManagerServices.getAllUsers();
+    this.users = users;
+    console.log(this.users);
   }
 
 }
