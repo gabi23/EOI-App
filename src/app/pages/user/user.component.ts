b366ac588688b004f4139240e3b8fda3d3691718
Guzman Hernandez <guzmanhernandez90@gmail.com>
2020-07-01 01:23:06 +0100

    Añade la capacidad de cargar desde la api los datos del  usuario especificado en la url por id

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiManagerService, User } from '../../services/api-manager.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+
+
 
 @Component({
-  selector: 'app-user',
+  selector: 'user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  user = {};
 
-  constructor() { }
+  constructor( private apiManager : ApiManagerService, private route: ActivatedRoute ) { 
+    this.loadUser();
+
+  }
+
 
   ngOnInit(): void {
   }
+
+  loadUser ()  {
+    this.apiManager.getUser(Number(this.route.snapshot.paramMap.get("id")))
+      .then((user) => {
+        this.user = user;
+       }).catch((err) => {
+         console.log (err);
+      });
+  }
+
+
 
 }
--- src/app/services/api-manager.service.ts
+++ src/app/services/api-manager.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { ResourceLoader } from '@angular/compiler';
+
+export type User = {
+  id?: number, 
+  name : string,
+  email : string,
+  type : string
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiManagerService {
-
+url = "http://localhost:3000/users"
   constructor() { }
+
+  public getUser(id : number) : Promise <User> {
+    return axios.get(`${this.url}/${id}`)
+      .then( res => res.data)
+    
+  }
 }
