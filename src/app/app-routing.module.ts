dea5bbffa8d29c22e96a96c620e4450a4e12a684
doramasma <doramas6@hotmail.com>
2020-07-01 23:47:01 +0100

    nav-bar css and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './pages/user/user.component';
 import { UsersComponent } from './pages/users/users.component';
-
+import {HomeComponent} from './pages/home/home.component';
 
   
 
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent },
   {path: 'users', component: UsersComponent},
-  { path: 'users/:id', component: UserComponent }
+  {path: 'users/:id', component: UserComponent }
 ];
 
 @NgModule({
