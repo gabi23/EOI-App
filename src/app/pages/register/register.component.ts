a2a9123ac6c5db87fd7a807f54000e07f1c95072
blueC4P <66735418+blueC4P@users.noreply.github.com>
2020-07-05 20:21:53 +0100

    New user and register basic validation with keyup Angular function

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
-import {ApiManagerService} from '../../services/api-manager.service';
-import {User} from '../../services/api-manager.service';
+import { ApiManagerService } from '../../services/api-manager.service';
+import { User } from '../../services/api-manager.service';
 
 
 @Component({
@@ -10,24 +10,48 @@ import {User} from '../../services/api-manager.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  
 
-  
+
+
   users: [] = [];
+
+  newUser: User = { name: "", email: "" }
+
+  errorNameBoolean: boolean = false;
   
-  newUser: User = {name : "", email : ""}
+  errorEmailBoolean: boolean = false;
+  
+  errorName: string = "";
 
-  constructor(private ApiManagerService: ApiManagerService) { 
-    
-   }
+  errorEmail: string= "";
 
-   
+  constructor(private ApiManagerService: ApiManagerService) {
 
-  ngOnInit(): void {}
+  }
 
-  addNewUser(){
+
+
+  ngOnInit(): void { }
+
+  addNewUser() {
     this.ApiManagerService.insertUser(this.newUser)
-    .then(i => i)
-    .catch((error) => {console.log(error)});
+      .then(i => i)
+      .catch((error) => { console.log(error) });
+  }
+  validationName() {
+    if (this.newUser.name.length < 2 || this.newUser.name.length > 30) {
+      this.errorNameBoolean = true;
+      this.errorName = "The name must have between 2 to 30 characters";
+    } else {
+      this.errorNameBoolean = false;
+    }
+  }
+  validationEmail() {
+    if (!this.newUser.email.includes("@")) {
+      this.errorEmailBoolean = true;
+      this.errorEmail = "The email isn't correct";
+    } else {
+      this.errorEmailBoolean = false;
+    }
   }
 }
