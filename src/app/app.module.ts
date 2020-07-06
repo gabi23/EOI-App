d2c1c37d02997a7a4341b802e1a16e53a850fbbe
doramasma <doramas6@hotmail.com>
2020-07-06 13:09:25 +0100

    solve courseSelect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DialogComponent } from './components/dialog/dialog.component';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatButtonModule} from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
@@ -32,6 +33,7 @@ import { MatInputModule } from '@angular/material/input';
     FormsModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatSelectModule,
     MatButtonModule,
     MatInputModule,
     BrowserAnimationsModule
