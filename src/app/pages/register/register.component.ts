5e655a8466b5b5ba6eb1c613e71d97f3f8720c1f
blueC4P <66735418+blueC4P@users.noreply.github.com>
2020-07-09 18:51:33 +0100

    just correct errors

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiManagerService } from '../../services/api-manager.service';
 import { User } from '../../services/api-manager.service';
 import { Course } from '../../services/api-manager.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,9 +11,15 @@ import { Course } from '../../services/api-manager.service';
 })
 export class RegisterComponent implements OnInit {
   
+  user: User;
+
+  courses: Course[];
+
+  newCourses: number[] = [];
+
   newCourse: Course = {name: "", studyField: "", description: ""};
 
-  newUser: User = {name : "", surname : "",  email : "" , role: "", courses: [], gitHubLogin: ""}
+  newUser: User = {name : "", surname : "",  email : "" , role: "", gitHubLogin: ""}
 
   emailValid = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
 
@@ -38,26 +45,62 @@ export class RegisterComponent implements OnInit {
 
   errorPhone: string = "";
 
-  constructor(private ApiManagerService: ApiManagerService) {
-
+  constructor(private apiManagerServices: ApiManagerService, private route: ActivatedRoute) {
+    this.loadCourses();
   }
 
   ngOnInit(): void { }
 
-  addNewCourse(){
-    this.ApiManagerService.insertCourse(this.newCourse)
-    .then(i => i)
-    .catch((error) => { console.log(error) });
+  isChecked(id: number): boolean{
+    return this.user.courses.includes(id);
   }
+
+  onCheckboxChange(e) {
+    if(e.target.checked) {
+      this.newCourses.push(parseInt(e.target.value));
+    }else {
+      let i: number = 0;
+      this.newCourses.forEach(courseId => {
+        if(courseId == e.target.value) {
+          this.newCourses.splice(i, 1);
+          return;
+        }
+        i++;
+      });
+    }
+    console.log(this.newCourses)
+  }
+
+  async loadUser(){
+    await this.apiManagerServices.getUser(Number(this.route.snapshot.paramMap.get("id")))
+      .then((user) => {
+        this.user = user;
+       }).catch((err) => {
+         console.log (err);
+      });
+    
+    this.user.courses.forEach(courseId => this.newCourses.push(courseId));
+  }
+
+  async loadCourses(): Promise<void> {
+    this.courses = await this.apiManagerServices.getCourses();
+  }
+
 
   feedBackNewCourseAdded(){
     return this.addedNewCourseValidation=true;
   }
 
   addNewUser() {
-    this.ApiManagerService.insertUser(this.newUser)
+    this.apiManagerServices.insertUser(this.newUser)
       .then(i => i)
       .catch((error) => { console.log(error) });
+  }
+
+  addNewCourse(){
+    this.apiManagerServices.insertCourse(this.newCourse)
+    .then(i => i)
+    .catch((error) => { console.log(error) });
   }
 
   feedBackNewUserAdded(){
